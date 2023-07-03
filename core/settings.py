73751da505686084cc30b2b8e022f558455c5600
Andrii <andremailboxua@gmail.com>
2023-07-03 21:00:38 +0300

    03-07-2023 Commit 3

--- core/settings.py
+++ core/settings.py
@@ -23,8 +23,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'malyn.business.work@gmail.com'
-EMAIL_HOST_PASSWORD = 'fmmezmlvyaifuaus'
+EMAIL_HOST_USER = 'novivorobischool@gmail.com'
+EMAIL_HOST_PASSWORD = 'oisbcuojrbrbssdx'
 
 # Application definition
 
