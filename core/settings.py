4e604e2ea37fbd196e0dae6a44c3e04aea79f116
Andrii <andremailboxua@gmail.com>
2023-07-06 07:34:40 +0300

    Error

--- core/settings.py
+++ core/settings.py
@@ -89,7 +89,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates'
+            # BASE_DIR / 'templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
