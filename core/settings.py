cea6a914077d105efbd7bde7ec1c970b9f29af91
Andrii <andremailboxua@gmail.com>
2023-07-06 09:41:29 +0300

    Some

--- core/settings.py
+++ core/settings.py
@@ -196,6 +196,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 }
 
 REST_AUTH = {
